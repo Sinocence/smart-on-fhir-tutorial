a81da816b80b92714beb45fe053964c986d979c7
Simon Nguyen <simon.nguyen@xmedius.com>
2018-10-15 11:41:09 -0400

    mend

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -9,6 +9,7 @@
 
     function onReady(smart)  {
       if (smart.hasOwnProperty('patient')) {
+
         var patient = smart.patient;
         var pt = patient.read();
         var obv = smart.patient.api.fetchAll({
@@ -129,6 +130,45 @@
       return undefined;
     }
   }
+
+  function setCookie(cname,cvalue,exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires=" + d.toGMTString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+      }
+
+      function getCookie(cname) {
+          var name = cname + "=";
+          var decodedCookie = decodeURIComponent(document.cookie);
+          var ca = decodedCookie.split(';');
+          for(var i = 0; i < ca.length; i++) {
+              var c = ca[i];
+              while (c.charAt(0) == ' ') {
+                  c = c.substring(1);
+              }
+              if (c.indexOf(name) == 0) {
+                  return c.substring(name.length, c.length);
+              }
+          }
+          return "";
+      }
+
+      function checkCookie() {
+          var user=getCookie("username");
+          if (user != "") {
+              alert("Welcome again " + user);
+          } else {
+             user = fhirClient.userId
+             if (user != "" && user != null) {
+                 setCookie("username", user, 30);
+             }
+          }
+      }
+
+
+
+
 
   window.drawVisualization = function(p) {
     $('#holder').show();
